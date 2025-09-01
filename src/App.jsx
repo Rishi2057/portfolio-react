e7cf4cba93b6311f58b959969c7d3aaf4b101450
Rishi2057 <rishisankar8@gmail.com>
2025-09-02 01:25:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -58,8 +58,8 @@ function App() {
         </div>
       </div>
       {/* portfolio */}
-
-      <div id='portfolio' className='min-h-fit mt-10 bg-[url("https://t4.ftcdn.net/jpg/04/21/44/29/360_F_421442912_e9dARIDF7CnBKKcB1Ooy0aNcEOJ13eVY.jpg")] py-5 md:py-15'>
+<div id='portfolio' className='pt-10'>
+<div  className='min-h-fit mt-10 bg-[url("https://t4.ftcdn.net/jpg/04/21/44/29/360_F_421442912_e9dARIDF7CnBKKcB1Ooy0aNcEOJ13eVY.jpg")] py-5 md:py-15'>
         <div className='flex justify-center'>
           <h1 className='px-10 py-1 border-4 text-3xl font-[500] text-gray-700'>PORTFOLIO</h1>
         </div>
@@ -83,6 +83,8 @@ function App() {
           <h1 className='text-white'>Many more to come.. </h1>
         </div>
       </div>
+</div>
+      
       {/* contact */}
       <div id='contact' className='h-screen'>
         <div className='pt-25 flex justify-center'>
